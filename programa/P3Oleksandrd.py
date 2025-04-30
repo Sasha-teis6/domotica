3d912d8b8d671e361750c3cddbb714806b6dba11
Sasha-teis6 <oleksandrdementiev@iesteis.net>
2025-04-30 14:26:38 +0200

    Create P3Oleksandrd.py

---
+++ programa/P3Oleksandrd.py
@@ -0,0 +1,23 @@
+"Programa 3"
+"Autor: Dementiev Oleksasndr"
+"Data: 30/04/2025"
+
+
+from microbit import *
+import music
+
+led = pin14
+i = 0
+
+while True:
+    if button_a.is_pressed():
+        for i in range(3):
+            i = i+1
+            led.write_digital(1)
+            sleep(500)
+            led.write_digital(0)
+            sleep(500)
+
+        for i in range(2):
+            music.play(music.RINGTONE)
+            sleep(1000)
