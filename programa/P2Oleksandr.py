51f2a930ab5d3eba769a22896fc87b1910a09d29
Sasha-teis6 <oleksandrdementiev@iesteis.net>
2025-04-30 14:04:00 +0200

    Create P2Oleksandr.py

---
+++ programa/P2Oleksandr.py
@@ -0,0 +1,14 @@
+from microbit import *
+
+led = pin14
+
+while True:
+    luz = pin1.read_analog()
+
+    if luz < 700:
+        led.write_digital(1)
+
+    else:
+        led.write_digital(0)
+
+    sleep(1000)
