a30c6a9f74570027e6eb78a1b5909782080a106f
Sasha-teis6 <oleksandrdementiev@iesteis.net>
2025-05-20 14:28:31 +0200

    Update P4POleksandr.py

--- programa/P4POleksandr.py
+++ programa/P4POleksandr.py
@@ -3,7 +3,20 @@
 "Data: 30/04/2025"
 from microbit import *
 
-pin2.set_analog_period(20)
+
+
+if button_b.is_pressed():        # Se se preme o botón B
+        if porta == 0:               # Se a porta está pechada
+            pin2.write_analog(90)    # Abrímosla
+            porta = 1                # Actualizamos o estado
+        else:                        # Se xa está aberta
+            pin2.write_analog(1)     # Pechámosla
+            porta = 0                # Actualizamos o estado
+            sleep(500) 
+
+
+
+"""pin2.set_analog_period(20)
 servo = pin2
 angulo_de_puerta = 0
 
@@ -15,5 +28,6 @@ while True:
          
         if angulo_de_puerta >= 90:
             angulo_de_puerta = 0
-            servo.write_analog(angulo_de_puerta)
+            servo.write_analog(angulo_de_puerta)"""
+
 
