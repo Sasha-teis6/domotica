375344b57e2cf22aee1749fa1bc07e1fb81b3274
Sasha-teis6 <oleksandrdementiev@iesteis.net>
2025-05-21 14:25:32 +0200

    Update P5Oleksandr.py

--- programa/P5Oleksandr.py
+++ programa/P5Oleksandr.py
@@ -6,15 +6,16 @@ from microbit import *
 import neopixel
 
 led = pin14
-np = neopixel.NeoPixel(pin13, 1)
-pir = pin15
+np = neopixel.NeoPixel(pin13, 1) # Configurar el neopixel en el pin 13
+pir = pin15 # Configurar el pin 15 para el sensor PIR
 
 while True:
-    if pir.read_digital() == 1:
+    if pir.read_digital() == 1: # Si el sensor PIR detecta movimiento?
 
-        for i in range(0, 1, 1):
+        for i in range(0, 1, 1): # Reproducir el tono RINGTONE una vez
             music.play(music.RINGTONE)
-
+       
+# Hacer parpadear el neopixel (rojo) 4 veces
        for i in range(0, 4, 1):
             np = (255, 0, 0)
             np.show()
@@ -23,20 +24,22 @@ while True:
             np.clear()
             sleep(500)
 
+
+# Hacer parpadear el LED 4 veces
         for i in range(0, 4, 1):
             led.write_digital(1)
             sleep(500)
             led.write_digital(0)
             sleep(500)
 
+# Mostrar una imagen de "enojado" en la pantalla
         display.show(Image.ANGRY)
         sleep(5000)
         display.clear()
 
 
 
-
-
+ # Si no hay movimiento, detener cualquier música que esté sonando
     else:
         music.stop()
     sleep(100)
