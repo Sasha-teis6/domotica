452826d5f95cf0e5a2b10c326e519d2751b09bf2
Sasha-teis6 <oleksandrdementiev@iesteis.net>
2025-05-21 14:18:28 +0200

    Update P3Oleksandrd.py

--- programa/P3Oleksandrd.py
+++ programa/P3Oleksandrd.py
@@ -2,17 +2,24 @@
 "Autor: Dementiev Oleksasndr"
 "Data: 30/04/2025"
 
-
 from microbit import *
 import music
 
 led = pin14
-i = 0
 
+while True:
+    if button_a.is_pressed(): # Si el botón A está presionado?
+        led.write_digital(1)  # Enciende el LED en 5 s
+        sleep(5000)
+        led.write_digital(0) # Apaga el LED 
+        sleep(500)
+        music.play(music.RINGTONE) # Reproduce musica RINGTONE en el altavoz del micro:bit
+        sleep(1000)
+
+"""
 while True:
     if button_a.is_pressed():
         for i in range(3):
-            i = i+1
             led.write_digital(1)
             sleep(500)
             led.write_digital(0)
@@ -21,3 +28,4 @@ while True:
         for i in range(2):
             music.play(music.RINGTONE)
             sleep(1000)
+"""
